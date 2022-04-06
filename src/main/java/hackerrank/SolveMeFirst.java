26d061df191739bd61a9e0f2263bec4c72f6d2f0
Makoto Yui <myui@apache.org>
2022-04-06 21:18:39 +0900

    Added hackerrank

---
+++ src/main/java/hackerrank/SolveMeFirst.java
@@ -0,0 +1,28 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * Complete the function solveMeFirst to compute the sum of two integers.
+ * 
+ * {@link https://www.hackerrank.com/challenges/solve-me-first/}
+ */
+public class SolveMeFirst {
+
+    static int solveMeFirst(int a, int b) {
+        // Hint: Type return a+b; below 
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            int a;
+            a = in.nextInt();
+            int b;
+            b = in.nextInt();
+            int sum;
+            sum = solveMeFirst(a, b);
+            System.out.println(sum);
+        }
+    }
+}
