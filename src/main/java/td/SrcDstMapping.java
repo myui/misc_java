7a5da280d1ca0ba8174ee8de7a4527da887451f4
Makoto Yui <myui@apache.org>
2022-04-08 18:28:50 +0900

    organized imports

--- src/main/java/td/SrcDstMapping.java
+++ src/main/java/td/SrcDstMapping.java
@@ -2,9 +2,7 @@ package td;
 
 import static java.util.Arrays.asList;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
